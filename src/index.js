aca7db560b5724890bf6ebff9a92fc88cdfd3c89
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-07-05 18:00:59 +0300

    feat: Add basic express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-//test
+const express = require('express');
+const app = express();
+
+app.set('port', 5000);
+
+const port = app.get('port');
+
+const server = app.listen(port, () =>
+  console.log(`Server started on port ${port}`)
+);
+
+module.exports = app;
